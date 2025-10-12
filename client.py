6fed1a1f271de0cba5ff83189e8bb9cc2ecfd254
csugetacc <twenke@csuchico.edu>
2025-10-12 15:45:25 -0700

    added keyboard functionality to client

--- client.py
+++ client.py
@@ -6,6 +6,7 @@ import csv
 import threading
 import json
 from pynput.mouse import Listener as MouseListener, Button
+from pynput.keyboard import Listener as KeyboardListener, Key
 
 hostname = "linux" # hardcoded laptop IPv4
 video_port = 5000
@@ -33,6 +34,7 @@ def recvall(sock, n):
 def client_program():
 
     host = getip(hostname)  # host ip
+    pressed_keys = set() # stores keystrokes to send
 
     # initalize for mouse window acounting
     window_dims = {'x': 0, 'y': 0, 'w': 1, 'h': 1}
@@ -43,10 +45,7 @@ def client_program():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as control_socket, \
          socket.socket(socket.AF_INET, socket.SOCK_STREAM) as video_socket:     
 
-        #mouse portion
-        print(f"Connecting to {host}:{control_port} ...")
-        control_socket.connect((host, control_port))
-
+        # mouse functions
         def send_mouse(command):
             try:
                 control_socket.sendall((json.dumps(command) + "\n").encode("utf-8"))
@@ -55,7 +54,6 @@ def client_program():
                 pass
 
         def on_move(x, y):
-
             # calculate for mouse in window
             with state_lock:
                 wx, wy, ww, wh = window_dims['x'], window_dims['y'], window_dims['w'], window_dims['h']
@@ -75,16 +73,49 @@ def client_program():
             if pressed:
                 send_mouse({'type': 'mouse_click', 'value': 'left' if button == Button.left else 'right'})
 
+        # keyboard functions
+        def key_to_name(key):
+            # assign char to key 
+            try:
+                if hasattr(key, 'char') and key.char is not None:
+                    return key.char
+            except AttributeError: # ignore non characters
+                pass
+            try:
+                return key.name  # get special keys name
+            except AttributeError:
+                return str(key)  # if all else fails turn key to string
+
+        def on_key_press(key):
+            name = key_to_name(key)
+            if name not in pressed_keys:    # only send once per press
+                pressed_keys.add(name)
+                send_mouse({'type': 'key_down', 'value': name})
+
+        def on_key_release(key):
+            name = key_to_name(key)
+            if name in pressed_keys:        # only delete once per release
+                pressed_keys.discard(name)
+            send_mouse({'type': 'key_up', 'value': name})
+
+
+        # start listners 
         listener = MouseListener(on_move=on_move, on_click=on_click)
         listener.start()
-
+        key_listener = KeyboardListener(on_press=on_key_press, on_release=on_key_release)
+        key_listener.start()
         
+        # connect control port
+        print(f"Connecting to {host}:{control_port} ...")
+        control_socket.connect((host, control_port))
+
         # video connection 
         print(f"Connecting to {host}:{video_port} ...")
         video_socket.connect((host, video_port))
         print("Connected. Press 'q' to quit.")
 
-        cv2.namedWindow("Remote Screen", cv2.WINDOW_NORMAL) # start window
+        # start window
+        cv2.namedWindow("Remote Screen", cv2.WINDOW_NORMAL) 
 
         while True:
             # read first 4 bytes
@@ -120,14 +151,13 @@ def client_program():
                 except Exception:
                     pass
 
-            
             # close connection
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
         # close out 
-        #finally:
         listener.stop()
+        key_listener.stop()
         cv2.destroyAllWindows()
 
 
